fb3164cc2c796b2ba3931a3448727517c934c88f
faryid <m.akbar.alfaryid@gmail.com>
2025-09-29 15:55:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const websocket = new WebSocketServer({ httpServer });
 let connection = null;
 
 httpServer.listen(3000, () => {
-  console.log("Listening at port 3000");
+  console.log("Listening at port 3000.");
 });
 
 websocket.on("request", (request) => {
